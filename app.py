ad7bec484e9b8d4f27fed8921a3ff527852beadb
Md Tanvir Hossain <md.tanvir.hossai.tusher>
2024-03-25 01:20:43 +0600

    class name added

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ import pickle
 from sklearn.preprocessing import StandardScaler
 
 
-# Load model prediction function
 def make_prediction(data):
-    # Scaling the input data
+
     scale = StandardScaler()
     X_test = scale.fit_transform(data)
 
@@ -15,32 +14,29 @@ def make_prediction(data):
     with open("model/iris_rf.pkl", "rb") as f:
         model = pickle.load(f)
 
-    # Make prediction
     y_test_pred = model.predict(X_test)
     return y_test_pred
 
 
-# Create Streamlit app
 def main():
     st.title("Iris Flower Prediction")
 
-    # Input fields for sepal length, width, petal length, and width
+    # Take input
     sepal_length = st.number_input("Sepal Length")
     sepal_width = st.number_input("Sepal Width")
     petal_length = st.number_input("Petal Length")
     petal_width = st.number_input("Petal Width")
 
-    # Prepare input data
     input_data = [[sepal_length, sepal_width, petal_length, petal_width]]
     input_df = pd.DataFrame(
         input_data,
         columns=["sepal_length", "sepal_width", "petal_length", "petal_width"],
     )
+    classes = {0: "Iris-setosa", 1: "Iris-versicolor", 2: "Iris-virginica"}
 
-    # Make prediction when 'Predict' button is clicked
     if st.button("Predict"):
         prediction = make_prediction(input_df)
-        st.write(f"Predicted class: {prediction[0]}")
+        st.write(f"Predicted class: {classes[prediction[0]]}")
 
 
 if __name__ == "__main__":
